5216c9fe74e34b44afe54a8fb05d27e9825fdda7
H1ieenrrr <trish20028@gmail.com>
2022-08-01 14:05:27 +0700

    Commit Thiết kế sơ đồ diagram, tạo project .net core cài đặt các nutget & setup

---
+++ LuckyDraw/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LuckyDraw.Models
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ LuckyDraw/Models/RoleModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LuckyDraw.Models
+{
+    [Table("Roles")]
+    public class RoleModel
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        [StringLength(20)]
+        public string RoleName { get; set; }
+    }
+}
