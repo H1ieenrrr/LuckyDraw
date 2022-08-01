2dc1156c0adfdf44b11e6fd365ac9264c940e840
H1ieenrrr <trish20028@gmail.com>
2022-08-01 18:35:47 +0700

    Commit Thêm khóa ngoại trong diagram

---
+++ LuckyDraw/Models/CampaignModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LuckyDraw.Models
+{
+    [Table("Campaigns")]
+    public class CampaignModel
+    {
+        [Key]
+        public int CampaignId { get; set; }
+
+        [ForeignKey("bulkModel")]
+        public int CampaignBulk { get; set; }
+
+        [ForeignKey("standalone")]
+        public int CampaignStandalone { get; set; }
+    }
+}
